296caf23bce26ccdf332533fa6dd09051e6c6fb2
FDatUCSD <fbinsyahrilakmar@ucsd.edu>
2025-06-10 21:43:27 -0700

    RF.sv: - Changed to use RdVal and RsVal for more consistent naming scheme

--- RF.sv
+++ RF.sv
@@ -2,15 +2,15 @@ module RF(
 	input  CLK, regWrite,
 	input [2:0] Rs, Rd,
 	input [7:0] writeValue,
-	output logic [7:0] val1, val2,
+	output logic [7:0] RsVal, RdVal,
 	output logic cmp
 	);
 
 	reg [7:0] registers_arr[0:7];
 
     	// Combinational reads
-	assign val1 = registers_arr[Rs];
-	assign val2 = registers_arr[Rd];
+	assign RsVal = registers_arr[Rs];
+	assign RdVal = registers_arr[Rd];
 	assign cmp = (registers_arr[Rs] == 8'b0); // for early branch resolution
 
    	 // Synchronous write to Rd
