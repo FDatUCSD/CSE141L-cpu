eb12b4257d9953a64e08e4fae224666007f14958
FDatUCSD <fbinsyahrilakmar@ucsd.edu>
2025-06-10 20:29:16 -0700

    RF.sv: - Renamed R1 and R2 to Rs and Rd respectively for clarity

--- RF.sv
+++ RF.sv
@@ -1,6 +1,6 @@
 module RF(
 	input  CLK, regWrite,
-	input [2:0] R1, R2,
+	input [2:0] Rs, Rd,
 	input [7:0] writeValue,
 	output logic [7:0] val1, val2,
 	output logic cmp
@@ -9,14 +9,14 @@ module RF(
 	reg [7:0] registers_arr[0:7];
 
     	// Combinational reads
-	assign val1 = registers_arr[R1];
-	assign val2 = registers_arr[R2];
-	assign cmp = (registers_arr[R1] == 8'b0); // for early branch resolution
+	assign val1 = registers_arr[Rs];
+	assign val2 = registers_arr[Rd];
+	assign cmp = (registers_arr[Rs] == 8'b0); // for early branch resolution
 
-   	 // Synchronous write to R2
+   	 // Synchronous write to Rd
     	always_ff @(posedge CLK) begin
         	if (regWrite) begin
-            		registers_arr[R2] <= writeValue;
+            		registers_arr[Rd] <= writeValue;
         	end
     	end
 
