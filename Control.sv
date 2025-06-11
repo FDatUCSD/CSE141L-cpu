26e0d69b62d6bc0b6955f63d5c3fb15b7c4cf499
FDatUCSD <fbinsyahrilakmar@ucsd.edu>
2025-06-10 18:44:49 -0700

    Control.sv: - Added ALUSrc, MemToReg - Combined all the ALU ops to one block - Made new typedef enum

--- Control.sv
+++ Control.sv
@@ -1,12 +1,25 @@
 module Control(
 	input init,
 	input [8:0] instruction,
-	output logic writeEnable, // write to RF
+	output logic writeEnable, // write to RF, 0 = not writing to reg file, 1 = write to reg file
 	output logic [2:0] OP, // ALU operation
-	output logic memRead, // load from memory
-	output logic memWrite, // store into memory
-	output logic branch // 1 if branching
+	output logic memRead, // load from memory, 0 = not reading from memory, 1 = read from memory
+	output logic memWrite, // store into memory, 0 = not writing to memory, 1 = writing to memory
+	output logic branch, // 1 if branching
+	output logic ALUSrc, // 0 = from register, 1 = immediate, only used for lw and sw
+	output logic MemToReg // 0 = ALU result, 1 = memory result
 	);
+
+	typedef enum logic [2:0] {
+		AND_OP  = 3'b000,
+		XOR_OP  = 3'b001,
+		SHL_OP  = 3'b010,
+		SHR_OP  = 3'b011,
+		ADD_OP  = 3'b100,
+		LW_OP   = 3'b101,
+		SW_OP   = 3'b110,
+		BR_OP   = 3'b111
+	} opcode_t;
 
 	always_comb begin
 		if(init) begin
@@ -15,60 +28,46 @@ module Control(
 			memRead = 0;
 			memWrite = 0;
 			branch = 0;
+			ALUSrc = 0;
+			MemToReg = 0;
 		end else begin
-			OP = instruction[8:6];
 
-			case(instruction[8:6])
-				3'b000: begin // and
-					writeEnable = 1;
-					memRead = 0;
-					memWrite = 0;
-					branch = 0;
-				end
+			opcode_t decoded_op;
+			decoded_op = instruction[8:6];
+			OP = decoded_op;
 
-				3'b001: begin // xor
+			case(OP)
+				AND_OP, XOR_OP, SHL_OP, SHR_OP, ADD_OP: begin // ALU op
 					writeEnable = 1;
 					memRead = 0;
 					memWrite = 0;
 					branch = 0;
+					ALUSrc = 0;
+					MemToReg = 0;
 				end
-
-				3'b010: begin // shl
-					writeEnable = 1;
-					memRead = 0;
-					memWrite = 0;
-					branch = 0;
-				end
-
-				3'b011: begin // shr
-					writeEnable = 1;
-					memRead = 0;
-					memWrite = 0;
-					branch = 0;
-				end
-				3'b100: begin // add
-					writeEnable = 1;
-					memRead = 0;
-					memWrite = 0;
-					branch = 0;
-				end
-				3'b101: begin // lw
+				LW_OP: begin // lw
 					writeEnable = 1;
 					memRead = 1;
 					memWrite = 0;
 					branch = 0;
+					ALUSrc = 1;
+					MemToReg = 1;
 				end
-				3'b110: begin // sw
+				SW_OP: begin // sw
 					writeEnable = 0;
 					memRead = 0;
 					memWrite = 1;
 					branch = 0;
+					ALUSrc = 1;
+					MemToReg = 0;
 				end
-				3'b111: begin // branch
+				BR_OP: begin // branch
 					writeEnable = 0;
 					memRead = 0;
 					memWrite = 0;
 					branch = 1;
+					ALUSrc = 0;
+					MemToReg = 0;
 				end
 
 				default: begin
@@ -76,6 +75,8 @@ module Control(
 					memRead = 0;
 					memWrite = 0;
 					branch = 0;
+					ALUSrc = 0;
+					MemToReg = 0;
 				end
 
 			endcase
